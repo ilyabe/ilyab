756cf1e556a8f3128eafd1476b03e5b69bc843ed
Ilya Bernshteyn <ilya.bernshteyn@guaranteedrate.com>
2017-12-15 09:00:54 -0600

    Send contact submissions via SES
    This emails out contact form submissions via SES to my email address.

--- src/clj/ilyab/email.clj
+++ src/clj/ilyab/email.clj
@@ -1,28 +1,42 @@
 (ns ilyab.email
-  "The functions for this namespace."
-  (:require [postal.core :refer [send-message]]))
+  "Functions for sending emails."
+  (:require [amazonica.aws.simpleemail :as ses]
+            [amazonica.core :refer [defcredential]]
+            [clojure.tools.logging :refer [error]]
+            [environ.core :refer [env]]
+            [postal.core :refer [send-message]]))
 
-(defn send-msg
-  "Function to take a subject and a message body `msg` and send them off to my
-  email address. Returns a map like this on success:
+(def email-key
+  "The AWS key for the SES user. Read from the EMAIL_KEY environment variable
+  in production or the `:email-key` in the `profiles.clj` locally."
+  (env :email-key))
 
-    {:message \"message sent\", :status \"OK\"}
+(def email-secret
+  "The AWS secret for the SES user. Read from the EMAIL_SECRET environment variable
+  in production or the `:email-key` in the `profiles.clj` locally."
+  (env :email-secret))
 
-  Returns a map like this on error:
+;; Convenience macro from amazonica to use the same credentials in all AWS calls
+;; in this namespace
+(defcredential email-key email-secret "us-east-1")
 
-    {:message \"failed to send message\", :status \"Error\"}
-  "
-  [s msg]
-  (let [res (send-message {:from "ilya@ilyab.com"
-                           :to "ilyabe@gmail.com"
-                           :subject s
-                           :body msg})]
-    (if (= :SUCCESS (:error res))
-      (-> res
-          (select-keys [:message])
-          (assoc :status "OK"))
-      (-> res
-          (select-keys [:message])
-          (assoc :status "Error")))))
+(defn send!
+  "Function to take two strings -- a subject `s` and a `body` and send an email
+  off to my email address. Returns `nil` if there's an error sending the email
+  via SES and likewise if either the subject or body are `nil`. If successful,
+  returns a map like this:
 
-;; TODO add logging
+    {:body \"hey\"
+     :message-id \"010001605a991020-bb67a6fe-5295-4a05-98a0-744a970b5dfc-000000\"
+     :status \"OK\"
+     :subject \"hey\"}
+
+  The from and to are hard-coded and there are no retries because I don't need
+  much more here right now."
+  [s body]
+  (if (and s body)
+    (let [resp (try (ses/send-email :destination {:to-addresses ["ilyabe@gmail.com"]}
+                                                  :source "ilya@ilyab.com"
+                                                  :message {:subject s, :body {:text body}})
+                 (catch Throwable t (error t)))]
+      (when resp (merge resp {:subject s, :body body, :status "OK"})))))
